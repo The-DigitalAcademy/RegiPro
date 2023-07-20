78b6a00476f87fa60cbf8d208ecdf9eda22a9a3c
KayAreBe <kjkoenaite@gmail.com>
2023-07-20 16:27:13 +0200

    second commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -16,7 +19,9 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/pages/sign-up/sign-up.component.ts
+++ frontend/src/app/pages/sign-up/sign-up.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit{
+
+  fullname: string = "";
+  lastname: string = "";
+  email: string = "";
+  password: string = "";
+
+  constructor(private http: HttpClient, private api : ApiService)
+  {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  signUp()
+  {
+    let bodyData = {
+      fullname: this.fullname,
+      lastname: this.lastname,
+      email: this.email,
+      password: this.password
+    };
+    
+  }
 
 }
