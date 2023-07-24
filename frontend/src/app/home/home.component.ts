9264375a934e36f1ca40800770dcd6e7eec6304b
DA Learner MAC M1 108 <dalearnermacm1108@DAs-MacBook-Air108.local>
2023-07-24 12:57:35 +0200

    Greeting user according to time of day functionality

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  name = 'Thabisile'
+  greetingUser: string = ''
 
   constructor() { }
 
   ngOnInit() {
+    this.greetingUser = this.greeting()
+    
   }
+    greeting() {
+      const date = new Date()
 
+const currentTime = date.getHours()
+
+// Morning 0 - 11
+// Noon 12 - 16
+// Evening 17 - 23
+if(currentTime >= 0 && currentTime <= 11) {
+    return `Good morning,` 
+} else if (currentTime >=12 && currentTime <= 16) {
+    return `Good afternoon,`
+} else if (currentTime >= 17 && currentTime <= 23) {
+    return `Good evening, `
+} else {
+    return ''
+}
+    }
 }
