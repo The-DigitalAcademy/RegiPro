511615fbfba80fd9b05847b6a085a28e9c7bacde
DA Learner MAC M1 108 <dalearnermacm1108@DAs-MacBook-Air108.local>
2023-07-25 08:06:39 +0200

    editted app.routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,31 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AuthGuard } from './guards/auth.guard';
 import { AboutComponent } from './about/about.component';
 import { LandingComponent } from './landing/landing.component';
 
-
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LandingComponent } from './landing/landing.component';
+
 
 const routes: Routes = [
   {path: '', redirectTo: 'landing', pathMatch:'full'},
   { path: 'signUp', component: SignUpComponent },
   { path: 'signIn', component: SignInComponent },
   {path: 'home', component: HomeComponent},
-  {path: 'landing', component: LandingComponent}
-
-const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
+  {path: 'landing', component: LandingComponent},
   {path: 'about', component: AboutComponent},
   {path: 'profile', component: ProfileComponent,canActivate:[AuthGuard]}
-
 ];
 
 @NgModule({
