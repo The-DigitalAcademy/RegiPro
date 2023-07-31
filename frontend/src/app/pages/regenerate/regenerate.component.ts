6c591f9b48ded1c9544365c6e0618ff13d465706
0ngezwa-felele <ongezwaf2@gmail.com>
2023-07-31 11:55:27 +0200

    services

--- frontend/src/app/pages/regenerate/regenerate.component.ts
+++ frontend/src/app/pages/regenerate/regenerate.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/regenerate.service';
 
 @Component({
   selector: 'app-regenerate',
   templateUrl: './regenerate.component.html',
-  styleUrls: ['./regenerate.component.scss']
+  styleUrls: ['./regenerate.component.scss'],
+  providers: [DataService] 
 })
-export class RegenerateComponent {
+export class RegenerateComponent implements OnInit {
+  data: any[] = []; 
+  item: any;
 
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.getDataFromServer();
+  }
+
+  getDataFromServer() {
+    this.dataService.getData().subscribe(
+      (response: any[]) => {
+        this.data = response;
+      },
+      (error: any[]) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
 }
+
+
