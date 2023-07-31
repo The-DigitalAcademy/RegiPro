42d84485ce264f5763765541be670cdbd0c7d1bf
Vutomi Luigi Khosa <72287282+VutomiKhosa-99@users.noreply.github.com>
2023-07-31 11:33:26 +0200

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -76,6 +76,7 @@ exports.signin = (req, res) => {
         res.status(200).send({
           id: user.id,
           email: user.email,
+          firstname: user.firstname,
           roles: authorities,
           accessToken: token
         });
