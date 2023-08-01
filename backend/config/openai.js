22da3466c96dff0524d3c75cb0ccc34bf3fc9e72
Vutomi Luigi Khosa <khosavutomi99@gmail.com>
2023-08-01 13:28:16 +0200

    Added generate function

--- backend/config/openai.js
+++ backend/config/openai.js
@@ -16,9 +16,21 @@ exports.handler = async (req, res) => {
         res.status(200).json({ text: response.data.choices[0].text })
     } else {
         res.status(200).json({ text: "Invalid prompt provided." })
-    }
-        
+    }    
         }
+
+        function generatePrompt(animal) {
+            const capitalizedAnimal =
+              animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+            return `Suggest three names for an animal that is a superhero.
+          
+          Animal: Cat
+          Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+          Animal: Dog
+          Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+          Animal: ${capitalizedAnimal}
+          Names:`;
+          }
   
         
       
