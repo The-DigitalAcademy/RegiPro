e300e4083611d07d77a63fc3d1df114bec654e45
Vutomi Luigi Khosa <khosavutomi99@gmail.com>
2023-08-03 14:35:33 +0200

    created a function for prompt

--- backend/config/openai.js
+++ backend/config/openai.js
@@ -1,36 +1,33 @@
-const { Configuration, OpenAIApi } = require("openai")
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI
-})
-const openai = new OpenAIApi(configuration)
+  apiKey: process.env.OPENAI,
+});
+const openai = new OpenAIApi(configuration);
 
 exports.handler = async (req, res) => {
-    if (typeof req.body.prompt === "string") {
-        const response = await openai.createCompletion({
-          model: "text-davinci-003",
-          prompt: req.body.prompt,
-          temperature: 0,
-          max_tokens: 1000
-        })
+  const { name, industry, description } = req.body
 
-        res.status(200).json({ text: response.data.choices[0].text })
-    } else {
-        res.status(200).json({ text: "Invalid prompt provided." })
-    }    
-        }
+  if (!name || !industry || !description) {
+    return res.status(400).json({ message: 'All fields are required' })
+  }
+  try  {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: req.body.prompt,
+      temperature: 0,
+      max_tokens: 4000,
+    });
 
-        function generatePrompt(animal) {
-            const capitalizedAnimal =
-              animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-            return `Suggest three names for an animal that is a superhero.
-          
-          Animal: Cat
-          Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-          Animal: Dog
-          Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-          Animal: ${capitalizedAnimal}
-          Names:`;
-          }
-  
-        
-      
+    res.status(200).json({ text: response.data.choices[0].text });
+  } catch(error) {
+    res.status(500).json({ message: "Internal server error." });
+  }
+};
+
+function generatePrompt(name, industry, description) {
+  return `Write a 3000 words business plan based on the following requirements
+        business name: ${name}
+        industry: ${industry}
+        about business: ${description}
+    .`;
+}
