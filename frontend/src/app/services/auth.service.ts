a53c22205e9c92c34356fab9cc9443efadd28f31
Thabo Chalatse <chalatsethabo@gmail.com>
2023-08-04 16:13:38 +0200

    changes

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://localhost:5001/auth/';
+const AUTH_API = 'http://localhost:5003/auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
