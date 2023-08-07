56e6429c60b4875743a51afaa3f8d30070c05eb4
KayAreBe <kjkoenaite@gmail.com>
2023-08-07 09:48:19 +0200

    home updates

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://localhost:5000/auth/';
+const AUTH_API = 'http://localhost:5001/auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
