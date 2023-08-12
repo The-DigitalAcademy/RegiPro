534e2ed4548436a2e7d23ea746823527dabc63d3
Vutomi Luigi Khosa <khosavutomi99@gmail.com>
2023-08-12 16:17:36 +0200

    Get all responses from a user

--- backend/middleware/verifySignup.js
+++ backend/middleware/verifySignup.js
@@ -15,21 +15,6 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
       });
       return;
     }
-
-    // // Email
-    // User.findOne({
-    //   where: {
-    //     email: req.body.email
-    //   }
-    // }).then(user => {
-    //   if (user) {
-    //     res.status(400).send({
-    //       message: "Failed! Email is already in use!"
-    //     });
-    //     return;
-    //   }
-
-    // });
       next();
   });
 };
