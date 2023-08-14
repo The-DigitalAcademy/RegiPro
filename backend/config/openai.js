dbb633d6a69634bd1c2896d3294e1c8f48db4fa5
Vutomi Luigi Khosa <khosavutomi99@gmail.com>
2023-08-14 11:59:37 +0200

    Business plan is formatted and downloaded

--- backend/config/openai.js
+++ backend/config/openai.js
@@ -3,7 +3,16 @@ const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
 const cloudinary = require("cloudinary");
-const { Document, Packer, Paragraph, TextRun } = require("docx");
+const docx = require("docx");
+const {
+  AlignmentType,
+  Document,
+  HeadingLevel,
+  Packer,
+  Paragraph,
+  TextRun,
+  UnderlineType,
+} = docx;
 const configuration = new Configuration({
   apiKey: process.env.OPENAI,
 });
@@ -24,27 +33,225 @@ if (fs.existsSync(path.join(__dirname, "..", "responses"))) {
   // console.log(business["Business Plan"]["Company Overview"]["Company Goals"]["Long-Term Goals"])
 
   const generateWordDoc = async () => {
+    let allParagraphs = [];
+
+    for (let i = 0; i < business.length; i++) {
+      const sectionParagraphs = [
+        new Paragraph({
+          text: business[i].section,
+          heading: HeadingLevel.HEADING_2,
+        }),
+        new Paragraph({
+          text: business[i].content,
+        }),
+        // ... (other paragraphs and content for the section)
+      ];
+
+      allParagraphs = allParagraphs.concat(sectionParagraphs);
+    }
+
     const doc = new Document({
+      // ... (document settings and styles)
       sections: [
         {
-          properties: {},
-          children: [
-            new Paragraph({
-              children: [
-                new TextRun({
-                  text: business["Business Plan"],
-                  bold: true,
-                }),
-                new TextRun({
-                  text: "\tGithub is the best",
-                  bold: true,
-                }),
-              ],
-            }),
-          ],
+          children: allParagraphs,
         },
       ],
     });
+
+    // for (let i = 0; i < business.length; i++) {
+    // const doc = new Document({
+    //   // sections: [
+    //   //   {
+    //   //     properties: {},
+    //   //     children: [
+    //   //       new Paragraph({
+    //   //         children: [
+    //   //           new TextRun({
+    //   //             text: business["Business Plan"],
+    //   //             bold: true,
+    //   //           }),
+    //   //           new TextRun({
+    //   //             text: "\tGithub is the best",
+    //   //             bold: true,
+    //   //           }),
+    //   //         ],
+    //   //       }),
+    //   //     ],
+    //   //   },
+    //   // ],
+
+    //   creator: "RegiPro Software",
+    //   title: "Sample Business Document",
+    //   description: "Generated business plan",
+    //   styles: {
+    //     paragraphStyles: [
+    //       {
+    //         id: "Heading1",
+    //         name: "Heading 1",
+    //         basedOn: "Normal",
+    //         next: "Normal",
+    //         quickFormat: true,
+    //         run: {
+    //           size: 28,
+    //           bold: true,
+    //           italics: true,
+    //           color: "red",
+    //         },
+    //         paragraph: {
+    //           spacing: {
+    //             after: 120,
+    //           },
+    //         },
+    //       },
+    //       {
+    //         id: "Heading2",
+    //         name: "Heading 2",
+    //         basedOn: "Normal",
+    //         next: "Normal",
+    //         quickFormat: true,
+    //         run: {
+    //           size: 26,
+    //           bold: true,
+    //           underline: {
+    //             type: UnderlineType.DOUBLE,
+    //             color: "FF0000",
+    //           },
+    //         },
+    //         paragraph: {
+    //           spacing: {
+    //             before: 240,
+    //             after: 120,
+    //           },
+    //         },
+    //       },
+    //       {
+    //         id: "aside",
+    //         name: "Aside",
+    //         basedOn: "Normal",
+    //         next: "Normal",
+    //         run: {
+    //           color: "999999",
+    //           italics: true,
+    //         },
+    //         paragraph: {
+    //           indent: {
+    //             left: 720,
+    //           },
+    //           spacing: {
+    //             line: 276,
+    //           },
+    //         },
+    //       },
+    //       {
+    //         id: "wellSpaced",
+    //         name: "Well Spaced",
+    //         basedOn: "Normal",
+    //         quickFormat: true,
+    //         paragraph: {
+    //           spacing: {
+    //             line: 276,
+    //             before: 20 * 72 * 0.1,
+    //             after: 20 * 72 * 0.05,
+    //           },
+    //         },
+    //       },
+    //       {
+    //         id: "ListParagraph",
+    //         name: "List Paragraph",
+    //         basedOn: "Normal",
+    //         quickFormat: true,
+    //       },
+    //     ],
+    //   },
+    //   numbering: {
+    //     config: [
+    //       {
+    //         reference: "my-crazy-numbering",
+    //         levels: [
+    //           {
+    //             level: 0,
+    //             format: "lowerLetter",
+    //             text: "%1)",
+    //             alignment: AlignmentType.LEFT,
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    //   sections: [
+    //     {
+    //       children: [
+    //         new Paragraph({
+    //           text: "Test heading1, bold and italicized",
+    //           heading: HeadingLevel.HEADING_1,
+    //         }),
+    //         new Paragraph({
+    //           text: business[i].section,
+    //           heading: HeadingLevel.HEADING_2,
+    //         }),
+    //         new Paragraph({
+    //           text: business[i].content,
+    //         }),
+    //         new Paragraph({
+    //           text: "Option1",
+    //           numbering: {
+    //             reference: "my-crazy-numbering",
+    //             level: 0,
+    //           },
+    //         }),
+    //         new Paragraph({
+    //           text: "Option5 -- override 2 to 5",
+    //           numbering: {
+    //             reference: "my-crazy-numbering",
+    //             level: 0,
+    //           },
+    //         }),
+    //         new Paragraph({
+    //           text: "Option3",
+    //           numbering: {
+    //             reference: "my-crazy-numbering",
+    //             level: 0,
+    //           },
+    //         }),
+    //         new Paragraph({
+    //           children: [
+    //             new TextRun({
+    //               text: "Some monospaced content",
+    //               font: {
+    //                 name: "Monospace",
+    //               },
+    //             }),
+    //           ],
+    //         }),
+    //         new Paragraph({
+    //           text: "An aside, in light gray italics and indented",
+    //           style: "aside",
+    //         }),
+    //         new Paragraph({
+    //           text: "This is normal, but well-spaced text",
+    //           style: "wellSpaced",
+    //         }),
+    //         new Paragraph({
+    //           children: [
+    //             new TextRun({
+    //               text: "This is a bold run,",
+    //               bold: true,
+    //             }),
+    //             new TextRun(" switching to normal "),
+    //             new TextRun({
+    //               text: "and then underlined ",
+    //               underline: {},
+    //             }),
+    //             new TextRun({
+    //               text: "and back to normal.",
+    //             }),
+    //           ],
+    //         }),
+    //       ],
+    //     },
+    //   ],
+    // });
 
     try {
       const buffer = await Packer.toBuffer(doc);
@@ -56,16 +263,16 @@ if (fs.existsSync(path.join(__dirname, "..", "responses"))) {
             console.error("Error uploading to Cloudinary:", error);
             throw error;
           }
-          console.log(result.url)
+          console.log(result.url);
           return result.url;
         })
         .end(buffer);
-
     } catch (error) {
       console.error("Error generating DOCX:", error);
-      throw error
+      throw error;
     }
   };
+
   generateWordDoc();
 }
 
@@ -78,7 +285,7 @@ exports.handler = async (req, res, next) => {
   try {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `Generate 3000-word business plan for ${name} in ${industry}, outlining ${description}. Return JavaScript object without variables, keeping consistent format for all plans.
+      prompt: `Generate 3000-word business plan for ${name} in ${industry}, outlining ${description}. Return JavaScript array with section and content object properties, keeping consistent format for all plans.
      `,
       temperature: 0,
       max_tokens: 4000,
--- backend/responses/response.js
+++ backend/responses/response.js
@@ -1,27 +1,51 @@
 module.exports = 
-{
-    "Business Plan": {
-        "Title": "Content Marketing Plan for Thabo in Marketing",
-        "Objective": "To create a comprehensive content marketing plan for Thabo in Marketing that will help him to reach his goals and objectives.",
-        "Overview": {
-            "Introduction": "Content marketing is a powerful tool for businesses to reach their target audience and build relationships with them. It is a form of marketing that focuses on creating and distributing valuable, relevant, and consistent content to attract and retain a clearly defined audience. Content marketing can be used to increase brand awareness, generate leads, and build customer loyalty. This plan will outline the steps necessary to create a successful content marketing strategy for Thabo in Marketing.",
-            "Goals and Objectives": "The goal of this content marketing plan is to create a comprehensive strategy that will help Thabo in Marketing reach his goals and objectives. The objectives of this plan are to: (1) create content that is relevant to Thabo's target audience; (2) develop a content distribution plan that will reach the target audience; (3) measure the success of the content marketing efforts; and (4) optimize the content marketing strategy for maximum effectiveness.",
-            "Target Audience": "The target audience for this content marketing plan is Thabo's current and potential customers. This includes individuals who are interested in the products and services offered by Thabo in Marketing."
-        },
-        "Content Creation": {
-            "Content Types": "The types of content that will be created for this content marketing plan include blog posts, articles, videos, infographics, ebooks, and webinars. Each type of content will be tailored to the target audience and will be designed to engage and educate them.",
-            "Content Strategy": "The content strategy for this content marketing plan will focus on creating content that is relevant to the target audience and that will help them to understand the products and services offered by Thabo in Marketing. The content will be created with the goal of educating the target audience and helping them to make informed decisions about the products and services offered by Thabo in Marketing.",
-            "Content Calendar": "A content calendar will be created to ensure that content is created and distributed on a regular basis. The content calendar will include the topics, types of content, and deadlines for each piece of content."
-        },
-        "Content Distribution": {
-            "Social Media": "Social media will be used to distribute the content created for this content marketing plan. The content will be shared on the various social media platforms that are used by the target audience. This will help to increase the reach of the content and ensure that it is seen by the target audience.",
-            "Email Marketing": "Email marketing will also be used to distribute the content created for this content marketing plan. The content will be sent to the email list of Thabo in Marketing's current and potential customers. This will help to increase the reach of the content and ensure that it is seen by the target audience.",
-            "Paid Advertising": "Paid advertising will be used to increase the reach of the content created for this content marketing plan. The content will be promoted on various platforms, such as Google Ads and Facebook Ads. This will help to ensure that the content is seen by the target audience."
-        },
-        "Content Measurement and Optimization": {
-            "Analytics": "Analytics will be used to measure the success of the content created for this content marketing plan. The analytics will provide insights into the performance of the content, such as the number of views, shares, and conversions. This will help to identify areas for improvement and optimize the content marketing strategy for maximum effectiveness.",
-            "Testing": "Testing will also be used to measure the success of the content created for this content marketing plan. A/B testing will be used to compare different versions of the content and determine which version is more effective. This will help to identify areas for improvement and optimize the content marketing strategy for maximum effectiveness."
-        },
-        "Conclusion": "This content marketing plan will help Thabo in Marketing to reach his goals and objectives. By creating content that is relevant to the target audience and distributing it through various channels, Thabo in Marketing will be able to increase brand awareness, generate leads, and build customer loyalty. The plan will also include analytics and testing to measure the success of the content and optimize the content marketing strategy for maximum effectiveness."
+[
+    {
+        section: 'Executive Summary',
+        content: `Crude in Construction is a roofing company that specializes in providing high-quality roofing services to residential and commercial customers. We are committed to providing our customers with the highest quality of service and materials, and our goal is to become the premier roofing company in the area. We will provide our customers with a wide range of services, including roof installation, repair, and maintenance. We will also offer a variety of roofing materials, including asphalt shingles, metal roofing, and tile roofing.
+
+Our team consists of experienced roofers who have been in the industry for many years. We have a strong commitment to customer service and satisfaction, and we strive to provide our customers with the best possible experience. We will also provide our customers with a warranty on all of our services and materials.
+
+We plan to market our services through a variety of methods, including online advertising, direct mail, and word-of-mouth. We will also work to build relationships with local contractors and suppliers to ensure that we are able to provide our customers with the best possible prices.
+
+Overall, Crude in Construction is committed to providing our customers with the highest quality of service and materials. We are confident that our commitment to customer service and satisfaction will help us to become the premier roofing company in the area.`
+    },
+    {
+        section: 'Company Description',
+        content: `Crude in Construction is a roofing company that specializes in providing high-quality roofing services to residential and commercial customers. We are based in [City], [State], and we serve customers throughout the [City] area.
+
+Our team consists of experienced roofers who have been in the industry for many years. We have a strong commitment to customer service and satisfaction, and we strive to provide our customers with the best possible experience. We are committed to providing our customers with the highest quality of service and materials, and our goal is to become the premier roofing company in the area.
+
+We offer a wide range of services, including roof installation, repair, and maintenance. We also offer a variety of roofing materials, including asphalt shingles, metal roofing, and tile roofing. We are committed to providing our customers with the best possible prices, and we work to build relationships with local contractors and suppliers to ensure that we are able to provide our customers with the best possible prices.
+
+We are committed to providing our customers with the highest quality of service and materials, and our goal is to become the premier roofing company in the area.`
+    },
+    {
+        section: 'Market Analysis',
+        content: `The roofing industry is a highly competitive market, and there are a number of companies that offer similar services. However, we believe that our commitment to customer service and satisfaction will help us to stand out from the competition.
+
+The [City] area is a growing market, and there is a high demand for roofing services. The population of the area is expected to continue to grow, and this will create additional demand for our services.
+
+We believe that our commitment to customer service and satisfaction will help us to stand out from the competition. We also believe that our competitive prices and wide range of services will help us to attract and retain customers.`
+    },
+    {
+        section: 'Marketing Plan',
+        content: `We plan to market our services through a variety of methods, including online advertising, direct mail, and word-of-mouth. We will also work to build relationships with local contractors and suppliers to ensure that we are able to provide our customers with the best possible prices.
+
+We will use online advertising to reach potential customers in the [City] area. We will use targeted ads to reach customers who are likely to be interested in our services. We will also use direct mail to reach potential customers in the area.
+
+We will also use word-of-mouth to spread the word about our services. We will work to build relationships with local contractors and suppliers, and we will encourage them to recommend our services to their customers. We will also use social media to reach potential customers and to build relationships with existing customers.`
+    },
+    {
+        section: 'Financial Plan',
+        content: `Our goal is to become the premier roofing company in the area, and we believe that our commitment to customer service and satisfaction will help us to achieve this goal. We plan to invest in the necessary equipment and materials to ensure that we are able to provide our customers with the highest quality of service and materials.
+
+We will also invest in marketing and advertising to ensure that we are able to reach potential customers. We will also invest in training and development to ensure that our team is able to provide our customers with the best possible experience.
+
+We anticipate that our revenues will increase as we grow our customer base. We also anticipate that our profits will increase as we become more efficient and as we are able to offer our customers competitive prices.`
+    },
+    {
+        section: 'Conclusion',
+        content: `Crude in Construction is committed to providing our customers with the highest quality of service and materials. We are confident that our commitment to customer service and satisfaction will help us to become the premier roofing company in the area. We plan to invest in the necessary equipment and materials to ensure that we are able to provide our customers with the highest quality of service and materials. We also plan to invest in marketing and advertising to ensure that we are able to reach potential customers. We are confident that our commitment to customer service and satisfaction will help us to achieve our goal of becoming the premier roofing company in the area.`
     }
-}
+]
