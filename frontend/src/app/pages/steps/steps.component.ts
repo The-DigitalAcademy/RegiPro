b3d7da717fd42d0bd029ffe67776d98290f7f866
0ngezwa-felele <ongezwaf2@gmail.com>
2023-08-14 09:32:00 +0200

    PDF viewer

--- frontend/src/app/pages/steps/steps.component.ts
+++ frontend/src/app/pages/steps/steps.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import PSPDFKit from 'pspdfkit';
 
 @Component({
   selector: 'app-steps',
   templateUrl: './steps.component.html',
   styleUrls: ['./steps.component.scss']
 })
-export class StepsComponent {
+export class StepsComponent implements AfterViewInit {
   currentStep = 1;
 
   nextStep(): void {
     this.currentStep++;
   }
 
-  
+  ngAfterViewInit() {
+    PSPDFKit.load({
+      baseUrl:
+        location.protocol + '//www.cipc.co.za/wp-content/uploads/Forms/Companies/CoR-14_1/' + location.host + '/assets/',
+      document: '/assets/CoR-14_1.pdf',
+      container: '#pspdfkit-container',
+    }).then((instance) => {
+      (window as any).instance = instance;
+    });
+  }
 }
