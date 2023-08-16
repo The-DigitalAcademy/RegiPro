ad5faaec1c444ba59feaa5931728f568d247eade
0ngezwa-felele <ongezwaf2@gmail.com>
2023-08-16 08:58:52 +0200

    updates

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const Role = db.role;
 
 // db.sequelize.sync();
 // force: true will drop the table if it already exists
-db.sequelize.sync({force: true}).then(() => {
+db.sequelize.sync({loggin: true}).then(() => {
     console.log('Drop and Resync Database with { force: true }');
     initial();
   });
